6489eaf9ad40c898e922fb9cbc02c4615ce51914
DemiusLs <dloss320@gmail.com>
2025-05-05 16:15:46 +0200

    feat: es 2 done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Esericizio 1
 
 const biciclette = [
     { nome: "Bianchi Oltre XR4", peso: 7.2 },
@@ -22,4 +23,33 @@ for (i = 0; i < biciclette.length; i++) {
 
 console.log(biciLight)
 
+// Esercizio 2
 
+const squadreCalcio = [
+    { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Napoli", puntiFatti: 0, falliSubiti: 0 }
+];
+
+let min = 0
+let max = 400
+
+for (i = 0; i < squadreCalcio.length; i++) {
+    squadreCalcio[i].puntiFatti = Math.floor(Math.random() * (max - min + 1)) + min;
+    squadreCalcio[i].falliSubiti = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(squadreCalcio);
+let newSquadre = [];
+
+for (i = 0; i < squadreCalcio.length; i++) {
+
+    newSquadre[i] = {
+        nome: squadreCalcio[i].nome,
+        falliSubiti: squadreCalcio[i].falliSubiti,
+    }
+}
+
+console.log(newSquadre)
